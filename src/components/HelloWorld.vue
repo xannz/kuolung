e035c3607e1c8865e302853732df13e28c7d41b0
Sander Borny <sander.borny@ugent.be>
2023-12-24 01:40:55 +0100

    readd dist to gitignore and install gh-pages module

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify 3 Beta
+          Welcome to Vuetify 3 Beta - Kuolung Jiu Jitsu
         </h1>
 
           <h4>Vite Preview</h4>
