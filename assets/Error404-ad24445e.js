578110df4242e5d86d1e3b4dc25458aa0a89327f
Sander Borny <sander.borny@ugent.be>
2024-01-03 22:38:07 +0100

    Updates

--- assets/Error404-ad24445e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c}from"./index-7186e356.js";const o={};function n(t,s){return r(),c("h1",null,"404")}const a=e(o,[["render",n]]);export{a as default};
--- assets/VSpacer-1976918f.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a}from"./index-7186e356.js";const r=a("v-spacer","div","VSpacer");export{r as V};
