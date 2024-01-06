1a5911376e7f8b2e125f968179e8e170394efe33
Sander Borny <sander.borny@ugent.be>
2024-01-06 22:48:06 +0100

    Updates

--- assets/Error404-4acc4e11.js
+++
@@ -1,1 +0,0 @@
-import{a_ as e,o as r,c}from"./index-24a98b84.js";const o={};function n(t,a){return r(),c("h1",null,"404")}const _=e(o,[["render",n]]);export{_ as default};
---
+++ assets/Error404-d18ad800.js
@@ -0,0 +1,1 @@
+import{a_ as e,o as r,c}from"./index-aa3e7742.js";const o={};function n(t,a){return r(),c("h1",null,"404")}const _=e(o,[["render",n]]);export{_ as default};
