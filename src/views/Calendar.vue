2a938fca22d2bd776298b784447364a3375d2169
thomas Crabbe <thomas.crabbe@student.howest.be>
2024-08-31 19:36:51 +0200

    add new calander dates 2024-2025
    (bug no september + last col has dates)

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -34,41 +34,77 @@ let today = ref([new Date()]);
 let events = ref(
     [
         {
-          title: 'Fighting Groep 1 (17:30-19:00)',
-          start: new Date('2024-01-03 17:30'),
-          end: new Date('2024-01-03 19:00'),
-          color: 'deep-purple',
+          title: 'Start traingingen jeugd',
+          start: new Date('2024-09-07 9:30'),
+          end: new Date('2024-09-07 12:30'),
+          color: colors.value[1],
         },
-        {
-          title: 'Fighting Groep 2 (19:00-20:30)', // Visual bug when on an half hour when the calendar is in week view
-          start: new Date('2024-01-03 9:30'),
-          end: new Date('2024-01-03 10:30'),
-          color: 'deep-purple',
-        },
-        {
-          title: 'Fighting Groep 1 (17:30-18:30)', // Visual bug when on an half hour when the calendar is in week view
-          start: new Date('2024-01-05 17:30'),
-          end: new Date('2024-01-05 18:30'),
-          color: 'deep-purple',
-        },
-        {
-          title: 'Fighting Groep 2 (18:30-20:30)',
-          start: new Date('2024-01-05 18:30'),
-          end: new Date('2024-01-05 20:30'),
-          color: 'deep-purple',
-        },
-        {
-          title: 'Fighting Alle (19:00-20:30)',
-          start: new Date('2024-01-06 19:00'),
-          end: new Date('2024-01-06 20:30'),
-          color: 'deep-purple',
-        },
-        {
-          title: 'Volwassenen (20:00-21:30)',
-          start: new Date('2024-01-8 20:00'),
-          end: new Date('2024-01-08 21:30'),
-          color: 'blue',
-        }
+      {
+        title: 'Start trainingen fighting',
+        start: new Date('2024-09-04 09:30'),
+        end: new Date('2024-09-04 12:30'),
+        color: colors.value[2],
+      },
+      {
+        title: 'Start trainingen volwassenen',
+        start: new Date('2024-09-02 09:30'),
+        end: new Date('2024-09-02 12:30'),
+        color: colors.value[0],
+      },
+      {
+        title: 'Vrijetijdsmarkt tuinen Scharpoord',
+        start: new Date('2024-09-15 10:00'),
+        end: new Date('2024-09-15 17:00'),
+        color: colors.value[3],
+      },
+      {
+        title: 'Spaghetti-avond',
+        start: new Date('2024-10-12 18:00'),
+        end: new Date('2024-10-12 23:00'),
+        color: colors.value[3],
+      },
+      {
+        title: 'Jeugd Sport Weekend',
+        start: new Date('2024-11-15 18:00'),
+        end: new Date('2024-11-17 17:00'),
+        color: colors.value[1],
+      },
+      {
+        title: 'Sinterklaas & familiedag (Laguna Duinbergen)',
+        start: new Date('2024-12-07 10:00'),
+        end: new Date('2024-12-07 18:00'),
+        color: colors.value[3],
+      },
+      {
+        title: 'Kuo Lung On Ice (schaatspiste A. Verweeplein)',
+        start: new Date('2024-12-21 19:00'),
+        end: new Date('2024-12-21 22:00'),
+        color: colors.value[3],
+      },
+      {
+        title: 'Let The Games Begin',
+        start: new Date('2025-03-01 10:00'), //datum nog niet gegeven
+        end: new Date('2025-03-31 17:00'),
+        color: colors.value[2],
+      },
+      {
+        title: 'Ardennen Weekend (16+, geboren in 2009 of eerder)',
+        start: new Date('2025-05-28 10:00'),
+        end: new Date('2025-06-02 17:00'),
+        color: colors.value[0],
+      },
+      {
+        title: 'Souper',
+        start: new Date('2025-06-21 18:00'),
+        end: new Date('2025-06-21 23:00'),
+        color: colors.value[3],
+      },
+      {
+        title: 'West-Vlaams Open',
+        start: new Date('2025-09-27 09:00'),
+        end: new Date('2025-09-28 18:00'),
+        color: colors.value[2],
+      }
       ]
 );
 </script>
